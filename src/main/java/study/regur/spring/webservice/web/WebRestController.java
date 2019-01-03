f26ec1cf7a2ad39ffd7e3fd563817a1b693eed34
StrongSoft <regur007@gmail.com>
2019-01-03 16:26:41 +0900

    테스트

--- src/main/java/study/regur/spring/webservice/web/WebRestController.java
+++ src/main/java/study/regur/spring/webservice/web/WebRestController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import study.regur.spring.webservice.domain.posts.PostsRepository;
+import study.regur.spring.webservice.service.PostsService;
 
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
-    private PostsRepository postsRepository;
+    private PostsService postsService;
 
     @GetMapping("/hi")
     public String hi(){
@@ -19,7 +19,7 @@ public class WebRestController {
     }
 
     @PostMapping("/posts")
-    public void savePosts(@RequestBody PostsSaveRequestDto dto){
-        postsRepository.save(dto.toEntity());
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
+        return postsService.save(dto);
     }
 }
