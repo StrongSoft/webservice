55e88af40c2fb4b09ec9d4eab0ac967cf6f9f0d6
StrongSoft <regur007@gmail.com>
2019-01-07 15:20:41 +0900

    테스트

--- src/main/java/study/regur/spring/webservice/web/WebRestController.java
+++ src/main/java/study/regur/spring/webservice/web/WebRestController.java
@@ -1,6 +1,7 @@
 package study.regur.spring.webservice.web;
 
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,11 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 import study.regur.spring.webservice.domain.dto.PostsSaveRequestDto;
 import study.regur.spring.webservice.service.PostsService;
 
+import java.util.Arrays;
+
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
     private PostsService postsService;
+    private Environment env;
 
     @GetMapping("/hi")
     public String hi(){
@@ -22,5 +26,10 @@ public class WebRestController {
     @PostMapping("/posts")
     public Long savePosts(@RequestBody PostsSaveRequestDto dto){
         return postsService.save(dto);
+    }
+
+    @GetMapping("profile")
+    public String getProfile(){
+        return Arrays.stream(env.getActiveProfiles()).findFirst().orElse("");
     }
 }
